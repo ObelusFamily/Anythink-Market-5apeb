26139c5724b75bad890125fecf277b2855b79012
Graeme Houston <graeme.houston@whitelabel-loyalty.com>
2022-09-20 18:48:08 +0100

    Fix render and run prettier

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import agent from '../agent';
-import { connect } from 'react-redux';
-import { ITEM_FAVORITED, ITEM_UNFAVORITED } from '../constants/actionTypes';
+import React from "react";
+import { Link } from "react-router-dom";
+import agent from "../agent";
+import { connect } from "react-redux";
+import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
 
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
@@ -32,23 +32,14 @@ const ItemPreview = (props) => {
   return (
     <div
       className="card bg-dark border-light p-3"
-      style={{ borderRadius: '20px' }}
+      style={{ borderRadius: "20px" }}
     >
-      {item.image ? (
-        <img
-          alt={item.title}
-          src={item.image}
-          className="card-img-top item-img"
-          style={{ borderRadius: '20px' }}
-        />
-      ) : (
-        <img
-          alt={item.title || 'item'}
-          src="/placeholder.png"
-          className="card-img-top item-img"
-          style={{ borderRadius: '20px' }}
-        />
-      )}
+      <img
+        alt={item.title}
+        src={item.image ? item.image : "/placeholder.png"}
+        className="card-img-top item-img"
+        style={{ borderRadius: "20px" }}
+      />
       <div className="card-body">
         <Link to={`/item/${item.slug}`} className="text-white">
           <h3 className="card-title">{item.title}</h3>
